1938b6bb5c9c7a6d849ed177ffd544ea7dd9e55a
YinZheng-Sun <57469177+YinZheng-Sun@users.noreply.github.com>
2021-01-06 21:02:32 +0800

    修改receive

--- fe/test/test_receive.py
+++ fe/test/test_receive.py
@@ -1,31 +1,59 @@
+import time
 import uuid
-
 import pytest
 
-from fe.access import auth
-from fe import conf
-from fe.access.new_buyer import register_new_buyer
-from fe.access.new_seller import register_new_seller
 from fe.test.gen_book_data import GenBook
-
+from fe.access.new_buyer import register_new_buyer
+from fe.access.book import Book
 
 class TestReceive:
     @pytest.fixture(autouse=True)
     def pre_run_initialization(self):
-        self.seller_id = "test_receive_seller_id_{}".format(str(uuid.uuid1()))
         self.store_id = "test_receive_store_id_{}".format(str(uuid.uuid1()))
+        self.seller_id = "test_receive_seller_{}".format(str(uuid.uuid1()))
+        self.store_id = "test_receive_book_store_id_{}".format(str(uuid.uuid1()))
         self.buyer_id = "test_receive_buyer_id_{}".format(str(uuid.uuid1()))
-        self.password = self.seller_id
+
         gen_book = GenBook(self.seller_id, self.store_id)
+        self.seller=gen_book.seller
         ok, buy_book_id_list = gen_book.gen(non_exist_book_id=False, low_stock_level=False, max_book_count=5)
         self.buy_book_info_list = gen_book.buy_book_info_list
         assert ok
+
+        self.password = self.buyer_id
         b = register_new_buyer(self.buyer_id, self.password)
+
         self.buyer = b
+        self.total_price = 0
+        for item in self.buy_book_info_list:
+            book: Book = item[0]
+            num = item[1]
+            self.total_price = self.total_price + book.price * num
+        code = self.buyer.add_funds(self.total_price+100000)
+        assert code == 200
+
         code, self.order_id = b.new_order(self.store_id, buy_book_id_list)
+        assert code == 200
+        code=b.payment(self.order_id )
+        assert code == 200
+        code = self.seller.deliver(self.seller_id, self.order_id)
         assert code == 200
         yield
 
-    def test_receive(self):
-        code = self.buyer.receive(self.order_id)
+    def test_ok(self):
+        code = self.buyer.receive(self.buyer_id,self.password,self.order_id)
         assert code == 200
+
+    def test_false_buyer(self):
+        code = self.buyer.receive(self.buyer_id+'s',self.password,self.order_id)
+        assert code != 200
+
+    def test_non_exist_order(self):
+        code = self.buyer.receive(self.buyer_id,self.password,self.order_id + 's')
+        assert code != 200
+
+    def test_repeat_receive_books(self):
+        code = self.buyer.receive(self.buyer_id,self.password,self.order_id)
+        assert code == 200
+        code = self.buyer.receive(self.buyer_id,self.password,self.order_id )
+        assert code != 200
